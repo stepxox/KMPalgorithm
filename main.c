b8b8aba5fc3db73ff0ad2e63cf0a39fdb339baee
stepxox <94843677+stepxox@users.noreply.github.com>
2022-05-01 21:00:51 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,33 @@
+int knuth(char * string, char * pattern)
+{
+    char *p1 = string;
+    char *p2 = pattern;
+    char *p3;
+    int i = 0;
+    int j = 0;
+    if(p1 == NULL || p2 == NULL || strlen(pattern) == 0 || strlen(string) == 0) return -1;
+    for(i = 0; i<strlen(string); i++)
+    {
+        if(*p1 == *p2)
+        {
+            p3 = p1;
+            for(j = 0;j<strlen(pattern);j++)
+            {
+                if(*p3 == *p2)
+                {
+                    p3++;p2++;
+                }
+                else
+                    break;
+            }
+            p2 = pattern;
+            if(j == strlen(pattern))
+            {
+
+                return i;
+            }
+        }
+        p1++;
+    }
+    return i;
+}
